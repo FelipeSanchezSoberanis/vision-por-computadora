59d921ce716a71b7f315e53b7d26446d0af0fc45
Felipe Sánchez Soberanis <felipesaso2000@gmail.com>
2022-09-14 21:43:11 -0500

    Contraharmonic mean

--- filtros_orden_estadistico/punto_1.py
+++ filtros_orden_estadistico/punto_1.py
@@ -5,6 +5,12 @@ import numpy as np
 from numpy.lib import math
 
 
+def contraharmonic_mean(pixels: list[int]) -> float:
+    return np.mean([pixel**2 for pixel in pixels], dtype="float") / np.mean(
+        pixels, dtype="float"
+    )
+
+
 def harmonic_mean(pixels: list[int]) -> float:
     return len(pixels) / np.sum([1 / pixel for pixel in pixels])
 
@@ -61,23 +67,27 @@ def print_images(kernel: int) -> None:
     image_mean: np.ndarray = filter_mean(image, kernel)
     image_geometric_mean: np.ndarray = apply_filter(image, kernel, geometric_mean)
     image_harmonic_mean: np.ndarray = apply_filter(image, kernel, harmonic_mean)
+    image_contraharmonic_mean: np.ndarray = apply_filter(
+        image, kernel, contraharmonic_mean
+    )
 
     images: dict[str, np.ndarray] = {
         "Original": image,
         "Mean": image_mean,
         "Geometric mean": image_geometric_mean,
         "Harmonic mean": image_harmonic_mean,
+        "Contraharmonic mean": image_contraharmonic_mean,
     }
 
     for i, (name, image) in enumerate(images.items()):
-        plt.subplot(1, 4, i + 1)
+        plt.subplot(2, 4, i + 1)
         plt.imshow(cv.cvtColor(image, cv.COLOR_BGR2RGB))
         plt.title(name)
         plt.axis("off")
 
 
 def main() -> None:
-    print_images(10)
+    print_images(3)
 
     #  axfreq = plt.axes([0.20, 0.1, 0.65, 0.03])
     #  kernel_slider = plt.Slider(
